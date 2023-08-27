f7a4a02c591582027ef5a79bfa324a7e7aa0d7e1
AlexandreMPDias <alexandrempdias@icloud.com>
2023-08-27 20:04:26 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.png";
+import "./App.css";
+
+function App() {
+	return (
+		<div>
+			<header></header>
+			<body>
+				<div className="AppLogoContainer">
+					<img src={logo} className="App-logo" alt="logo" />
+				</div>
+				<input type="text" />
+				<div>
+					<button></button>
+				</div>
+			</body>
+		</div>
+	);
+}
+
+export default App;
